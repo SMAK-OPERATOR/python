5b7ca0c71c9b0e32a3e33e02e8a9e293618dea45
SMAK-OPERATOR <andrey--01@mail.ru>
2022-04-15 16:32:09 +0300

    1 & 2 final

--- 1lab/second.py
+++ 1lab/second.py
@@ -1,21 +1,22 @@
 import configparser
+import argparse
 from configparser import ConfigParser
+from Classes import User
+import os
 
-
-class User:
-    def __init__(self, login, password, name, surname):
-        self.login = login
-        self.password = password
-        self.name = name
-        self.surname = surname
-
-    def __str__(self):
-        return f"Login = {self.login}, password = {self.password}, name = {self.name}, surname = {self.surname}"
-
+parser = argparse.ArgumentParser(description="Argument parser for path argument")
+parser.add_argument('-p', '--path', default='Settings\\second.ini', help="Path to ini file")
+args = parser.parse_args()
 
 config = ConfigParser()
-config.read("second.ini")
+config.read(args.path)
+
+if not os.path.exists("Settings"):
+    os.mkdir("\\Settings")
+    print(f"Folder {os.getcwd()}\\Settings has been created")
+
 user1 = User("Login1", "Password1", "Name1", "Surname1")
+
 try:
     config.add_section("section_user2")
     config.set("section_user2", "login", "login2")
@@ -26,32 +27,37 @@ try:
 except configparser.DuplicateSectionError:
     print("User2 already in file")
 
+with open(args.path, "w") as f:
+    config.write(f)
+
+config2 = ConfigParser()
+config2.read(args.path)
+
 user2 = User(
-    # config.get("section_user2", "login"),
-    # config.get("section_user2", "password"),
-    # config.get("section_user2", "name"),
-    # config.get("section_user2", "surname")
-    config["section_user2"]["login"],
-    config["section_user2"]["password"],
-    config["section_user2"]["name"],
-    config["section_user2"]["surname"]
+    # config2.get("section_user2", "login"),
+    # config2.get("section_user2", "password"),
+    # config2.get("section_user2", "name"),
+    # config2.get("section_user2", "surname")
+    config2["section_user2"]["login"],
+    config2["section_user2"]["password"],
+    config2["section_user2"]["name"],
+    config2["section_user2"]["surname"]
 )
-print(user2)
+# print(user2)
 users = [user1, user2]
 i = 0
 for user in users:
     i += 1
     try:
-        config.add_section(f"section_user{i}")
-        config.set(f"section_user{i}", "login", str(user.login))
-        config.set(f"section_user{i}", "password", str(user.password))
-        config.set(f"section_user{i}", "name", str(user.name))
-        config.set(f"section_user{i}", "surname", str(user.surname))
+        config2.add_section(f"section_user{i}")
+        config2.set(f"section_user{i}", "login", str(user.login))
+        config2.set(f"section_user{i}", "password", str(user.password))
+        config2.set(f"section_user{i}", "name", str(user.name))
+        config2.set(f"section_user{i}", "surname", str(user.surname))
         print(f"User{i} was added to file")
     except configparser.DuplicateSectionError:
-        print("User2 already in file")
-with open("second.ini", "w") as f:
-    config.write(f)
+        print(f"User{i} already in file")
 
-print(config.keys())
+with open(args.path, "w") as f:
+    config2.write(f)
 
